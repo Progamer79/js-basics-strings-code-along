7aae8ef6d74ad2bee1754cd14fac3aa4ea9ac92e
Progamer79 <cboyles66@yahoo.com>
2021-03-31 12:34:40 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Chris Boyles';
+
+const welcomeMessage = 'Welcome to Flatbook, Chris Boyles!'  
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, +${C}!";
+
+
+
+
+
+
+    
